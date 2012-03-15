63ddf67c4ac9bf2f542f0c794bf77fd88a985e56
Ezequiel Rivadeneira Lichardi <ezerivadeneiral@gmail.com>
2012-03-15 16:55:09 -0300

    Metodo de Romberg - Funcionando

--- main.pas
+++ main.pas
@@ -6,7 +6,7 @@ interface
 
 uses
   Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, StdCtrls,
-  Menus;
+  Menus, formula, math, Grids;
 
 type
 
@@ -22,16 +22,22 @@ type
     Label2: TLabel;
     Label3: TLabel;
     MainMenu1: TMainMenu;
+    Matriz: TStringGrid;
     MemoResultado: TMemo;
+    Formula: TArtFormula;
     procedure ButtonCalcularClick(Sender: TObject);
     procedure EditPeriodoChange(Sender: TObject);
     procedure FormCreate(Sender: TObject);
     procedure Label1Click(Sender: TObject);
     procedure Label2Click(Sender: TObject);
+
   private
     { private declarations }
   public
+    procedure CalcularAn(funcion:String; periodo:Double);
+    function func(x:Double):Double;
     { public declarations }
+
   end; 
 
 var
@@ -44,8 +50,18 @@ implementation
 { TFormMain }
 
 procedure TFormMain.ButtonCalcularClick(Sender: TObject);
+var
+  T :Double;
 begin
-
+  if (EditPeriodo.Text = 'Pi') then
+  begin
+       T := 3.14;
+  end
+  else
+  begin
+       T:= StrToFloat(EditPeriodo.Text)
+  end;
+  CalcularAn(EditEcuacion.Text, T );
 end;
 
 procedure TFormMain.EditPeriodoChange(Sender: TObject);
@@ -66,6 +82,77 @@ end;
 procedure TFormMain.Label2Click(Sender: TObject);
 begin
 
+end;
+
+// Calcula el coeficiente An.
+// @Parametros: Funcion, Periodo, n?
+procedure TFormMain.CalcularAn(funcion:String; periodo:Double);
+var
+  fa, fb, T, R0, a, b, h, sum: Double;
+  n, k, m: Integer;
+begin
+  {         a=T/2
+     An = Integral f(t).cos [(2nPi/T)t] dt
+           b=-T/2
+  }
+  T :=  periodo;
+  a := T/2; b := -T/2;
+  fa := FormMain.func(a);
+  fb := FormMain.func(b);
+  //Approximate the definite integral of f from a to b by Romberg's method.
+  //    eps is the desired accuracy."""
+  Matriz.Cells[0,0] := FloatToStr( (0.5)*( a - b ) * (fa+fb) );  //R[0][0]
+  n := 1;
+  While True do
+  begin
+       h := b - a / (2**n); // h = hn
+       Matriz.RowCount := Matriz.RowCount + 1; //Agregamos una fila vacia
+       sum := 0;
+       for k := 1 to 2**(n-1) do
+           sum := sum + func( a + (2*k+1)*h );  // Sumatoria
+       Matriz.Cells[0,n] := FloatToStr((0.5)*StrToFloat(Matriz.Cells[n-1,0])+(h*sum));  //R[n][0]
+
+       for m:=1 to n+1 do
+       begin
+           Matriz.ColCount:= Matriz.ColCount +1;
+           if (Matriz.Cells[m-1,n-1] = '') then
+              Matriz.Cells[m-1,n-1] := '0';
+           Matriz.Cells[m,n] := FloatToStr( StrToFloat(Matriz.Cells[m-1,n]) + ( StrToFloat(Matriz.Cells[m-1,n]) - StrToFloat(Matriz.Cells[m-1,n-1] ) )/  ((4**m)-1) );
+       end;
+
+       if abs( StrToFloat(Matriz.Cells[n-1,n]) - StrToFloat(Matriz.Cells[n,n]) ) < 1 Then
+       begin
+          MemoResultado.Append(Matriz.Cells[n,n]);
+          Break;
+       end;
+       n := n + 1;
+  end;
+end;
+
+{ Funcion que calcula el valor de f(a) siendo a = cte }
+function TFormMain.func(x:Double):Double;
+var
+  // Vector de variables. Contiene las variables.
+  variables: array of string;
+  //Vector de valores. Contiene los valores de las variables, como ser, x=10, y=2, etc.
+  vecVariables: TCalcArray;
+  //Entero que especifica la cantidad de variables que uso
+  cantidadVariables: Integer;
+  s: String;
+begin
+     FormMain.Formula.CaseSensitive:= True; //Nos indica si es sensible a Mayusculas y Minusculas
+     FormMain.Formula.TestUsedVars:= True; //Comprueba las variables
+     FormMain.Formula.NoLeadingZero:= True; //Considera o no el cero a la derecha
+     FormMain.Formula.ZeroEmptyString:= True; //Considerar o no a la funcion vacia como cero
+     FormMain.MemoResultado.Append('Calculando f('+FormMain.EditPeriodo.Text+')');
+     cantidadVariables := 1;
+     SetLength(vecVariables, cantidadVariables);
+     SetLength(variables, cantidadVariables);
+     variables[0] := 'x';
+     setN(vecVariables[0],x);
+     //Realizamos el calculo de la funcion y agregamos el resultado al Memo.
+     s := FormMain.Formula.ComputeStr(FormMain.EditEcuacion.Text, cantidadVariables, @variables, @vecVariables);
+     func := StrToFloat(s);
 end;
 
 end.
