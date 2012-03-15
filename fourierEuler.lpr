dadc8e51dc4f2c5f84e4b499509f0124a69ba70d
Ezequiel Rivadeneira Lichardi <ezerivadeneiral@gmail.com>
2012-03-15 12:40:09 -0300

    GUI Basica

--- fourierEuler.lpr
+++ fourierEuler.lpr
@@ -14,7 +14,7 @@ uses
 
 begin
   Application.Initialize;
-  Application.CreateForm(TForm1, Form1);
+  Application.CreateForm(TFormMain, FormMain);
   Application.Run;
 end.
 
--- main.pas
+++ main.pas
@@ -1,14 +1,33 @@
-unit main; 
+unit main;
 
 {$mode objfpc}{$H+}
 
 interface
 
 uses
-  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs; 
+  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, StdCtrls,
+  Menus;
 
 type
-  TForm1 = class(TForm)
+
+  { TFormMain }
+
+  TFormMain = class(TForm)
+    ButtonCalcular: TButton;
+    EditCantCompoenentes: TEdit;
+    EditEcuacion: TEdit;
+    EditPeriodo: TEdit;
+    GroupBox1: TGroupBox;
+    Label1: TLabel;
+    Label2: TLabel;
+    Label3: TLabel;
+    MainMenu1: TMainMenu;
+    MemoResultado: TMemo;
+    procedure ButtonCalcularClick(Sender: TObject);
+    procedure EditPeriodoChange(Sender: TObject);
+    procedure FormCreate(Sender: TObject);
+    procedure Label1Click(Sender: TObject);
+    procedure Label2Click(Sender: TObject);
   private
     { private declarations }
   public
@@ -16,11 +35,38 @@ type
   end; 
 
 var
-  Form1: TForm1; 
+  FormMain: TFormMain;
 
 implementation
 
 {$R *.lfm}
+
+{ TFormMain }
+
+procedure TFormMain.ButtonCalcularClick(Sender: TObject);
+begin
+
+end;
+
+procedure TFormMain.EditPeriodoChange(Sender: TObject);
+begin
+
+end;
+
+procedure TFormMain.FormCreate(Sender: TObject);
+begin
+
+end;
+
+procedure TFormMain.Label1Click(Sender: TObject);
+begin
+
+end;
+
+procedure TFormMain.Label2Click(Sender: TObject);
+begin
+
+end;
 
 end.
 
