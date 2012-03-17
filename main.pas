21c8bc1513ffb5593770d0d2b270c04b4727eb43
Ezequiel Rivadeneira Lichardi <ezerivadeneiral@gmail.com>
2012-03-17 14:43:45 -0300

    Calculo de A0

--- main.pas
+++ main.pas
@@ -33,8 +33,9 @@ type
 
   private
     { private declarations }
+    SerieF: String;
   public
-    procedure CalcularAn(funcion:String; periodo:Double);
+    procedure CalcularAn(funcion:String; periodo:Double; subN: Integer);
     function func(x:Double):Double;
     { public declarations }
 
@@ -61,7 +62,7 @@ begin
   begin
        T:= StrToFloat(EditPeriodo.Text)
   end;
-  CalcularAn(EditEcuacion.Text, T );
+  CalcularAn(EditEcuacion.Text, T , 0);   //Calculo A0
 end;
 
 procedure TFormMain.EditPeriodoChange(Sender: TObject);
@@ -86,47 +87,59 @@ end;
 
 // Calcula el coeficiente An.
 // @Parametros: Funcion, Periodo, n?
-procedure TFormMain.CalcularAn(funcion:String; periodo:Double);
+procedure TFormMain.CalcularAn(funcion:String; periodo:Double; subN: Integer);
 var
-  fa, fb, T, R0, a, b, h, sum: Double;
+  fa, fb, T, R0, a, b, h, sum, a0: Double;
   n, k, m: Integer;
 begin
-  {         a=T/2
-     An = Integral f(t).cos [(2nPi/T)t] dt
-           b=-T/2
+  {             a=T/2
+     An = 2/T Integral f(t).cos [(2nPi/T)t] dt
+               b=-T/2
   }
   T :=  periodo;
-  a := T/2; b := -T/2;
+  b := T/2; a := -T/2;
   fa := FormMain.func(a);
   fb := FormMain.func(b);
-  //Approximate the definite integral of f from a to b by Romberg's method.
-  //    eps is the desired accuracy."""
-  Matriz.Cells[0,0] := FloatToStr( (0.5)*( a - b ) * (fa+fb) );  //R[0][0]
-  n := 1;
-  While True do
+  Matriz.RowCount := 1;
+  Matriz.ColCount:= 1;
+  if (subN = 0) then
   begin
-       h := b - a / (2**n); // h = hn
-       Matriz.RowCount := Matriz.RowCount + 1; //Agregamos una fila vacia
-       sum := 0;
-       for k := 1 to 2**(n-1) do
-           sum := sum + func( a + (2*k+1)*h );  // Sumatoria
-       Matriz.Cells[0,n] := FloatToStr((0.5)*StrToFloat(Matriz.Cells[n-1,0])+(h*sum));  //R[n][0]
+       //Approximate the definite integral of f from a to b by Romberg's method.
+       //    eps is the desired accuracy."""
+       Matriz.Cells[0,0] := FloatToStr( (0.5)*( b - a ) * (fa+fb) );  //R[0][0]
 
-       for m:=1 to n+1 do
+       n := 1;
+       While True do
        begin
-           Matriz.ColCount:= Matriz.ColCount +1;
-           if (Matriz.Cells[m-1,n-1] = '') then
-              Matriz.Cells[m-1,n-1] := '0';
-           Matriz.Cells[m,n] := FloatToStr( StrToFloat(Matriz.Cells[m-1,n]) + ( StrToFloat(Matriz.Cells[m-1,n]) - StrToFloat(Matriz.Cells[m-1,n-1] ) )/  ((4**m)-1) );
+            h := (b - a) / (2**n); // h = hn
+            MemoResultado.Append( FloatToStr(h));
+            Matriz.RowCount := Matriz.RowCount + 1; //Agregamos una fila vacia
+            sum := 0;
+            for k := 1 to 2**(n-1) do
+                sum := sum + func( a + (2*k-1)*h );  // Sumatoria
+            Matriz.Cells[0,n] := FloatToStr( (0.5)*StrToFloat(Matriz.Cells[n-1,0])+(h*sum) );  //R[n][0]
+
+            for m:=1 to n+1 do
+            begin
+                 Matriz.ColCount:= Matriz.ColCount +1;
+                 if ( Matriz.Cells[m-1,n-1] = '') then
+                    Matriz.Cells[m-1,n-1] := '0';
+                 Matriz.Cells[m,n] := FloatToStr( StrToFloat(Matriz.Cells[m-1,n]) + ( StrToFloat(Matriz.Cells[m-1,n]) - StrToFloat(Matriz.Cells[m-1,n-1] ) )/  ((4**m)-1) );
+            end;
+
+            if abs( StrToFloat(Matriz.Cells[n-1,n]) - StrToFloat(Matriz.Cells[n,n]) ) < 0.00000001 Then
+            begin
+                 a0 := StrToFloat(Matriz.Cells[n,n])*2/T;
+                 if ( a0 <> 0 ) then
+                    SerieF := FloatToStr(a0);
+                 MemoResultado.Append('A0 = '+FloatToStr(a0));
+                 Break;
+            end;
+            n := n + 1;
        end;
 
-       if abs( StrToFloat(Matriz.Cells[n-1,n]) - StrToFloat(Matriz.Cells[n,n]) ) < 1 Then
-       begin
-          MemoResultado.Append(Matriz.Cells[n,n]);
-          Break;
-       end;
-       n := n + 1;
   end;
+
 end;
 
 { Funcion que calcula el valor de f(a) siendo a = cte }
@@ -144,7 +157,6 @@ begin
      FormMain.Formula.TestUsedVars:= True; //Comprueba las variables
      FormMain.Formula.NoLeadingZero:= True; //Considera o no el cero a la derecha
      FormMain.Formula.ZeroEmptyString:= True; //Considerar o no a la funcion vacia como cero
-     FormMain.MemoResultado.Append('Calculando f('+FormMain.EditPeriodo.Text+')');
      cantidadVariables := 1;
      SetLength(vecVariables, cantidadVariables);
      SetLength(variables, cantidadVariables);
